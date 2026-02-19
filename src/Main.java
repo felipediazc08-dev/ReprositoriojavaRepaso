6c3abc4c4ff80df76e963cd51bc14fb1e0b5892a
AndresD <felipediazc08@gmail.com>
2026-02-19 12:29:54 -0500

    Subir cambios

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Andres Felipe Diaz Cortes");
+        System.out.println("Cuadra Los Chilacos");
+        System.out.println("18");
     }
 }
